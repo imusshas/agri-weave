14abefc175b747c5f440ba9fd135a46fb4bdf8ad
Rezaul Karim <sumon29@student.sust.edu>
2024-09-11 04:07:23 +0600

    r

---
+++ back-end/Agri-Weave-v1/src/main/java/Rezaul/Muhib/agriweavev1/AgriWeaveV1Application.java
@@ -0,0 +1,13 @@
+package Rezaul.Muhib.agriweavev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriWeaveV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriWeaveV1Application.class, args);
+	}
+
+}
---
+++ back-end/Agri-Weave-v1/src/test/java/Rezaul/Muhib/agriweavev1/AgriWeaveV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Rezaul.Muhib.agriweavev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriWeaveV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
